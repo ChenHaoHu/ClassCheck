a4b41ae3525bbf1433b022d8235ece9fd750fed0
hcy <775656764@qq.com>
2018-08-30 13:59:15 +0800

    添加数据综合查看
    添加数据综合查看

--- cms/index.js
+++ cms/index.js
@@ -20,6 +20,22 @@ var time = 0;
 onloaddata = function (data) {
     //  $("#list").html(" ")
     var str = "";
+
+    var class1_success_sign = 0;
+    var class1_fail_sign = 0;
+    var class1_success_rest = 0;
+    var class1_fail_rest = 0;
+    var class2_success_sign = 0;
+    var class2_fail_sign = 0;
+    var class2_success_rest = 0;
+    var class2_fail_rest = 0;
+    var class3_success_sign = 0;
+    var class3_fail_sign = 0;
+    var class3_success_rest = 0;
+    var class3_fail_rest = 0;
+
+    var classname = "未知";
+
     // console.log(data)
     for (let index = 0; index < data.length; index++) {
     //     var signtime = parseInt( data[index].time.split(":")[1])*60+parseInt( data[index].time.split(":")[2]);
@@ -28,16 +44,83 @@ onloaddata = function (data) {
     //    console.log("注册时间："+signtime)
     //    console.log("创建时间："+createtime)
     //    console.log("有效时间："+time)
+console.log(data[index].stuid.slice(7,8))
+        if(data[index].stuid.slice(7,8)==1){
+            if(data[index].signstate == "签到成功"){
+                class1_success_sign ++;
+            }
+            if(data[index].signstate == "签到超时"){
+                class1_fail_sign ++;
+            }
+            if(data[index].signstate == "请假成功"){
+                class1_success_rest ++;
+            }
+            if(data[index].signstate == "请假超时"){
+                class1_fail_rest ++;
+            }
+            classname = "一班"
+        }
+        
+        if(data[index].stuid.slice(7,8)==2){
+            if(data[index].signstate == "签到成功"){
+                class2_success_sign ++;
+            }
+            if(data[index].signstate == "签到超时"){
+                class2_fail_sign ++;
+            }
+            if(data[index].signstate == "请假成功"){
+                class2_success_rest ++;
+            }
+            if(data[index].signstate == "请假超时"){
+                class2_fail_rest ++;
+            }
+            classname = "二班"
+        }
+        
+        if(data[index].stuid.slice(7,8)==3){
+            if(data[index].signstate == "签到成功"){
+                class3_success_sign ++;
+            }
+            if(data[index].signstate == "签到超时"){
+                class3_fail_sign ++;
+            }
+            if(data[index].signstate == "请假成功"){
+                class3_success_rest ++;
+            }
+            if(data[index].signstate == "请假超时"){
+                class3_fail_rest ++;
+            }
+            classname = "三班"
+        }
+
 
             var con = " <tr><td>" + data[index].name +
             "</td><td >" + data[index].stuid + "</td><td>" + data[index].distance + "m</td>"
             + "<td>" + data[index].signstate + "</td>"
+            + "<td>" + classname + "</td>"
+            + "<td>" + (data.length - index) + "</td>"
             + " <td><span class='badge badge-info'>" + data[index].time + "</span></td></tr>"
        
         str = str + con;
         // $("#list").append(con)
     }
     $("#list").html(str)
+
+    $("#class1_success_sign").text(class1_success_sign)
+    $("#class1_fail_sign").text(class1_fail_sign)
+    $("#class1_success_rest").text(class1_success_rest)
+    $("#class1_fail_rest").text(class1_fail_rest)
+
+    $("#class2_success_sign").text(class2_success_sign)
+    $("#class2_fail_sign").text(class2_fail_sign)
+    $("#class2_success_rest").text(class2_success_rest)
+    $("#class2_fail_rest").text(class2_fail_rest)
+
+    $("#class3_success_sign").text(class3_success_sign)
+    $("#class3_fail_sign").text(class3_fail_sign)
+    $("#class3_success_rest").text(class3_success_rest)
+    $("#class3_fail_rest").text(class3_fail_rest)
+
 }
 
 
